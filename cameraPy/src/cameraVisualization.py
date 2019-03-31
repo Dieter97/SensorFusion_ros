d371ac997c0ca0247231f629e62532c8aa4f90c3
Dieter Balemans <dieter.balemans@hotmail.com>
2019-03-31 16:36:52 +0200

    Added messages and cameraVisualization script

---
+++ cameraPy/src/cameraVisualization.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import os
+import sys
+# import roslib
+# roslib.load_manifest('my_package')
+import time
+
+import cv2
+import rospy
+import message_filters
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
+from std_msgs.msg import Header
+from sensor_fusion_msg.msg import CameraObjects
+from sensor_fusion_msg.msg import ObjectBoundingBox
+
+
+class image_converter:
+
+    def __init__(self):
+        self.image_pub = rospy.Publisher("/camera/detection/out/image", Image)
+
+        self.bridge = CvBridge()
+        self.image_sub = message_filters.Subscriber("/carla/ego_vehicle/camera/rgb/front/image_color", Image, queue_size = 10)
+        self.prediction_seb = message_filters.Subscriber("/camera/detection/out/", CameraObjects, queue_size=1)
+        self.ts = message_filters.ApproximateTimeSynchronizer([self.image_sub, self.prediction_seb], 10, slop=0.01)
+        self.ts.registerCallback(self.callback)
+
+    def callback(self, image, predictions):
+            try:
+                cv_image = self.bridge.imgmsg_to_cv2(image, "bgr8")
+            except CvBridgeError as e:
+                print(e)
+
+            cv_image = self.drawPredicions(cv_image, predictions)
+
+            try:
+                self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+            except CvBridgeError as e:
+                print(e)
+
+    def drawPredicions(self,image, predictions):
+        for prediction in predictions.bounding_boxes:
+            x = prediction.xmin
+            y = prediction.xmax
+            w = prediction.ymin
+            h = prediction.ymax
+            cv2.rectangle(image,(x-w,y-h),(x+w,y+h),(0,255,0),2)
+            font = cv2.FONT_HERSHEY_PLAIN
+            cv2.putText(image,prediction.Class,(x-w,y-h-5),font,2,(255,255,0))
+        return image
+
+
+def main(args):
+    ic = image_converter()
+    rospy.init_node('image_converter', anonymous=True)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main(sys.argv)
--- cameraPy/src/object-detection.py
+++ cameraPy/src/object-detection.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
+import os
+import sys
 # import roslib
 # roslib.load_manifest('my_package')
 import time
 
-import numpy as np
-import sys
-import rospy
 import cv2
-from std_msgs.msg import String
-from sensor_msgs.msg import Image
+import rospy
 from cv_bridge import CvBridge, CvBridgeError
-import os.path
-
-import sys, os
+from sensor_msgs.msg import Image
+from std_msgs.msg import Header
+from sensor_fusion_msg.msg import CameraObjects
+from sensor_fusion_msg.msg import ObjectBoundingBox
 sys.path.append(os.path.join(os.getcwd(),'python/'))
 
 import darknet as dn
@@ -22,13 +21,13 @@ import darknet as dn
 class image_converter:
 
     def __init__(self):
-        self.image_pub = rospy.Publisher("/camera/detection/out", Image)
+        self.image_pub = rospy.Publisher("/camera/detection/out", CameraObjects)
 
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("/carla/ego_vehicle/camera/rgb/front/image_color", Image, self.callback,queue_size=1)
+        self.image_sub = rospy.Subscriber("/carla/ego_vehicle/camera/rgb/front/image_color", Image, self.callback, buff_size=100, queue_size=30)
         # Give the configuration and weight files for the model and load the network using them.
-        modelConfiguration = b"/home/dieter/darknet/cfg/yolov3.cfg"
-        modelWeights = b"/home/dieter/darknet/data/yolov3.weights"
+        modelConfiguration = b"/home/dieter/darknet/cfg/yolov2-tiny.cfg"
+        modelWeights = b"/home/dieter/darknet/data/yolov2-tiny.weights"
 
         dn.set_gpu(0)
 
@@ -41,27 +40,32 @@ class image_converter:
         except CvBridgeError as e:
             print(e)
 
-
         # Save image on temporary location for object detection
         cv2.imwrite('/tmp/tmp.png', cv_image)
-        #im = self.array_to_image(cv_image)
-        #dn.rgbgr_image(im)
         start = time.time()
         r = dn.detect(self.net, self.meta, b"/tmp/tmp.png")
         end = time.time()
-        #r = dn.detect2(self.net, self.meta, im)
-        #cv_image = self.drawPredicions(cv_image,r,0.6)
-
+        self.generateMsg(r, 0.6)
 
         print("YOLO prediction took %f seconds" % (end - start))
 
-        #cv2.imshow("Image window",  self.cv_image)
-        #cv2.waitKey(3)
-
-        try:
-            self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-        except CvBridgeError as e:
-            print(e)
+    def generateMsg(self, predictions, threshold):
+        objects = []
+        for prediction in predictions:
+            if prediction[1] > threshold:
+                object = ObjectBoundingBox()
+                object.Class = prediction[0]
+                object.probability = prediction[1]
+                object.xmin = int(prediction[2][0])
+                object.xmax = int(prediction[2][1])
+                object.ymin = int(prediction[2][2])
+                object.ymax = int(prediction[2][3])
+                objects.append(object)
+        objectMsg = CameraObjects()
+        objectMsg.header = Header()
+        objectMsg.header.stamp = rospy.Time.now()
+        objectMsg.bounding_boxes = objects
+        self.image_pub.publish(objectMsg)
 
     def drawPredicions(self,image,predictions,threshold):
         for prediction in predictions:
